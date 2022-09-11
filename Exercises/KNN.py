a21b05f6dff0824ff64543fc6bf790c25320397d
OnlyourMiracle <2440310664@qq.com>
2022-09-11 17:04:34 +0800

    old code

---
+++ Exercises/KNN.py
@@ -0,0 +1,37 @@
+from numpy import *
+import operator
+
+def createDataSet():
+    group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
+    labels = ['A', 'A', 'B', 'B']
+    return group, labels
+
+def classifyo(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    diffMat = tile(inX, (dataSetSize, 1)) - dataSet
+    sqDiffMat = diffMat**2
+    sqDistances = sqDiffMat.sum(axis=1)
+    distances = sqDistances**0.5
+    sortedDistIndicies = distances.argsort()
+    classCount={}
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel, 0)
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+def file2matrix(filename):
+    fr = open(filename)
+    arrayOLines = fr.readlines()
+    numberOfLines = len(arrayOLines)
+    returnMat = zeros((numberOfLines, 3))
+    classLabelVector = []
+    index = 0
+    for line in arrayOLines:
+        line = line.strip()
+        listFormLine = line.split('\t')
+        returnMat[index, :] = listFormLine[0:3]
+        labels = {'didntLike':1,'smallDoses':2,'largeDoses':3}
+        classLabelVector.append(labels[listFormLine[-1]])
+        index += 1
+    return returnMat, classLabelVector
